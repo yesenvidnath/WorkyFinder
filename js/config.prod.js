af3f946776a85841fc4d4b012c2faba7506a0c92
Yesen Kandalama <ykandalama@gmail.com>
2025-10-12 10:26:34 +0530

    Add GitHub Pages workflow and production config

---
+++ js/config.prod.js
@@ -0,0 +1,8 @@
+// Production Google Calendar API credentials
+const CLIENT_ID = 'YOUR_CLIENT_ID';    // Example: '123456789-abcdef.apps.googleusercontent.com'
+const API_KEY = 'YOUR_API_KEY';        // Example: 'AIzaSyA_BCdefghijklmno-123456789'
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC_URL = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
